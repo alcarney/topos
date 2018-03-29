39c0f870941dbc98c2cd6b797cd3c690af520d14
Alex Carney <alcarneyme@gmail.com>
2018-03-29 19:59:34 +0100

    Topos is now a namespace package
    - This *should* allow us to distribute parts of the core topos
      distribution as separate packages :D

--- tests/test_codefu.py
+++ tests/test_codefu.py
@@ -1,12 +1,10 @@
 from unittest.mock import patch
-
-
-from topos.codefu import prepare_vertex_array_function
+from topos.core.codefu import prepare_vertex_array_function
 
 
 class TestPrepareFVertexArray(object):
 
-    @patch('topos.codefu.raiseError')
+    @patch('topos.core.codefu.raiseError')
     def test_num_args_check(self, Err):
 
         prepare_vertex_array_function(lambda x, y, r, t: 4)
